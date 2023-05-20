91deae4ff8b7297973b53f7625e772ff881ea0e0
Matías Marambio <46638059+Vee-v@users.noreply.github.com>
2023-05-20 18:12:13 -0400

    Update main.py
    Actualizado para borrador entrega 2

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import gurobipy as gp
 from modelador import *
 
 
-def main(n_ex, n_tec, t_mes):
+def main(n_ex, n_tec, t_mes, n_pl, n_m, n):
     ### FUNCION OBJETIVO ###
-    modelo, obj = modelador(n_ex, n_tec, t_mes)
+    modelo, obj = modelador(n_ex, n_tec, t_mes, n_pl, n_m, n)
     modelo.update()
     modelo.setObjective(obj, gp.GRB.MINIMIZE)
     modelo.optimize()
     # imprime los coeficientes de las variables
-    # modelo.printAttr('obj')
+#     modelo.printAttr('obj')
     # imprime los valores de las variables
     modelo.printAttr('X')
     return
     
 if __name__ == "__main__":
-    main(n_ex=5, n_tec=5, t_mes=36)
+    main(n_ex=5, n_tec=5, t_mes=36, n_pl=3, n_m=3, n='Restricciones Entrega 2 V1')
